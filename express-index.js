840437f5cb7c49fe358eb647fc0c9974c4c93014
Ubuntu <ubuntu@ip-172-31-26-253.us-east-2.compute.internal>
2020-03-28 22:05:46 +0000

    fixing a line that was unnecessary

--- express-index.js
+++ express-index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-express.static(static, [options])
-
 app.get('/', (req, res) => res.send('Welcome to the Beginnings of MethCode'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
