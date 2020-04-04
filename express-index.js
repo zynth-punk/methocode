c7af7877c582b0f86af0b7d72488e2c5a7c305f2
brunokoppel@gmail.com <bruno.koppel@gmail.com>
2020-04-04 12:37:38 -0400

    reagrouping files

--- express-index.js
+++ express-index.js
@@ -5,7 +5,9 @@ const port = 3000
 var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/mainmenu.html'));
+    res.sendFile(path.join(__dirname + '/PagesTree//HTML/mainmenu.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(path.join(__dirname, '/PagesTree/CSS/')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))s
