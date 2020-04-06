a7a075108b55d529c9b0ded34758f84fc317fb1f
brunokoppel@gmail.com <bruno.koppel@gmail.com>
2020-04-06 13:17:57 -0400

    testing git ignore

---
+++ data/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb://127.0.0.1:3000/MethoCode', {useNewURLParser: true})
+    .catch(e => {console.error('Connection error', e.message)
+                })
+
+const db = mongoose.connection
+
+module.exports = db
