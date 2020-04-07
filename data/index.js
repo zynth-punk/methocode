4b1e91655c47e4e116b5d8a1f8550301db07369e
brunokoppel@gmail.com <bruno.koppel@gmail.com>
2020-04-06 23:09:02 -0400

    testing controllers, routers, and models

--- data/index.js
+++ data/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose
-    .connect('mongodb://127.0.0.1:3000/MethoCode', {useNewURLParser: true})
-    .catch(e => {console.error('Connection error', e.message)
-                })
+mongoose.connect('mongodb://localhost:27017/methocode', { useNewUrlParser: true, useUnifiedTopology: true } ).then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
+
 
 const db = mongoose.connection
 
--- express-index.js
+++ express-index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const db = require('./data')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 var path = require('path');
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/PagesTree/')));
+
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/PagesTree//HTML/mainmenu.html'));
+    res.sendFile(path.join(__dirname + '/PagesTree/HTML/mainmenu.html'));
 });
 
-app.use(express.static(path.join(__dirname, '/PagesTree/')));
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
