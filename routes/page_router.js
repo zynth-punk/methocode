149bd3471fe03f7f7c34d4ef5daf8a841dee292d
brunokoppel@gmail.com <bruno.koppel@gmail.com>
2020-04-07 23:07:33 -0400

    renaming pages and updating router

--- routes/page_router.js
+++ routes/page_router.js
@@ -7,35 +7,35 @@ var path = require('path');
 app.use(express.static(path.join(__dirname , '/public')));
 
 router.get('/index', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/index.html')); 
+  res.sendFile(path.join(__dirname , '../views/index.html')); 
 })
 
 router.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/login.html')); 
+  res.sendFile(path.join(__dirname , '../views/login.html')); 
 })
 
 router.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/about.html')); 
+  res.sendFile(path.join(__dirname , '../views/about.html')); 
 })
 
 router.get('/help', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/help.html')); 
+  res.sendFile(path.join(__dirname , '../views/help.html')); 
 })
 
 router.get('/profile', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/profile.html')); 
+  res.sendFile(path.join(__dirname , '../views/profile.html')); 
 })
 
 router.get('/register', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/register.html')); 
+  res.sendFile(path.join(__dirname , '../views/register.html')); 
 })
 
 router.get('/codetranslator', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/codetranslator.html')); 
+  res.sendFile(path.join(__dirname , '../views/codetranslator.html')); 
 })
 
 router.get('/mainmenu', function (req, res) {
-  res.sendFile(path.join(__dirname , '../pages/HTML/mainmenu.html')); 
+  res.sendFile(path.join(__dirname , '../views/mainmenu.html')); 
 })
 router.get('/', function(req, res) { res.redirect('/mainmenu');  } );
 
